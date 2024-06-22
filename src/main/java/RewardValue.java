5d6e5f6716cc4f7cb5362f7d6583f294dbca4aec
Omar Maldonado <144171824+Omar-B-Maldonado@users.noreply.github.com>
2024-06-21 23:08:16 -0400

    Added Unit Tests Added two unit tests to check the accuracy of the conversions from cash to miles and from miles to cash via the conversion rate MILES_TO_CASH_RATE of 0.0035

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -4,29 +4,29 @@ public class RewardValue {
     private int milesValue;
     private final double MILES_TO_CASH_RATE = 0.0035;
 
-    public RewardValue(double cashValue){
+    public RewardValue(double cashValue) {
         this.cashValue  = cashValue;
         this.milesValue = convertToMiles(cashValue);
     }
 
-    public RewardValue(int milesValue){
+    public RewardValue(int milesValue) {
         this.milesValue = milesValue;
         this.cashValue  = convertToCash(milesValue);
     }
 
-    public double getCashValue(){
+    public double getCashValue() {
         return cashValue;
     }
 
-    public int getMilesValue(){
+    public int getMilesValue() {
         return milesValue;
     }
 
-    private double convertToCash(int miles){
+    public double convertToCash(int miles) {
         return miles * MILES_TO_CASH_RATE;
     }
 
-    private int convertToMiles(double cash){
-        return (int)Math.floor(cash / MILES_TO_CASH_RATE);
+    public int convertToMiles(double cash) {
+        return (int)(cash / MILES_TO_CASH_RATE);
     }
 }
--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 3.59;
+        var rewardValue = new RewardValue(cashValue);
+        int expectedMiles = 1025;
+        assertEquals(expectedMiles, rewardValue.convertToMiles(cashValue));
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 1025;
+        var rewardValue = new RewardValue(milesValue);
+        double expectedCash = 3.5875;
+        assertEquals(expectedCash, rewardValue.convertToCash(milesValue));
     }
 }
